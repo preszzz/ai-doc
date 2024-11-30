aecf7ac32c3f41f86b8b3c83f8f5cdf613a0e3ed
Pres <pres@Mac-mini.local>
2024-11-29 23:55:20 -0500

    new files

---
+++ utils/routes-map.ts
@@ -0,0 +1,281 @@
+import {EachRoute} from "@/lib/routes-config";
+
+const ROUTES: EachRoute[] = [
+    {
+        title: "Getting Started",
+        href: "/getting-started",
+        noLink: true,
+        items: [
+            { title: "Introduction", href: "/introduction" },
+            {
+                title: "Installation",
+                href: "/installation",
+            },
+            { title: "Quick Start Guide", href: "/quick-start-guide" },
+            {
+                title: "Project Structure",
+                href: "/project-structure",
+            },
+            {
+                title: "Components",
+                href: "/components",
+                items: [
+                    { title: "Stepper", href: "/stepper" },
+                    { title: "Tabs", href: "/tabs" },
+                    { title: "Note", href: "/note" },
+                    { title: "Code Block", href: "/code-block" },
+                    { title: "Image & Link", href: "/image-link" },
+                    { title: "Custom", href: "/custom" },
+                ],
+            },
+            { title: "Themes", href: "/themes" },
+            {
+                title: "Customize",
+                href: "/customize",
+            },
+        ],
+    },
+    {
+        title: "Math",
+        href: "/math",
+        noLink: true,
+        items: [
+            {
+                title: "Linear Algebra",
+                href: "/linear-algebra",
+                items: [
+                    { title: "Stepper", href: "/stepper" },
+                    { title: "Tabs", href: "/tabs" },
+                    { title: "Note", href: "/note" },
+                    { title: "Code Block", href: "/code-block" },
+                    { title: "Image & Link", href: "/image-link" },
+                    { title: "Custom", href: "/custom" },
+                ],
+            },
+            {
+                title: "Calculus",
+                href: "/calculus",
+                items: [
+                    { title: "Stepper", href: "/stepper" },
+                    { title: "Tabs", href: "/tabs" },
+                    { title: "Note", href: "/note" },
+                    { title: "Code Block", href: "/code-block" },
+                    { title: "Image & Link", href: "/image-link" },
+                    { title: "Custom", href: "/custom" },
+                ],
+            },
+            {
+                title: "Statistics",
+                href: "/statistics",
+                items: [
+                    { title: "Stepper", href: "/stepper" },
+                    { title: "Tabs", href: "/tabs" },
+                    { title: "Note", href: "/note" },
+                    { title: "Code Block", href: "/code-block" },
+                    { title: "Image & Link", href: "/image-link" },
+                    { title: "Custom", href: "/custom" },
+                ],
+            },
+        ],
+    },
+    {
+        title: "Machine Learning",
+        href: "/machine-learning",
+        noLink: true,
+        items: [
+            {
+                title: "Statistical Learning",
+                href: "/statistical-learning",
+                items: [
+                    { title: "Stepper", href: "/stepper" },
+                    { title: "Tabs", href: "/tabs" },
+                    { title: "Note", href: "/note" },
+                    { title: "Code Block", href: "/code-block" },
+                    { title: "Image & Link", href: "/image-link" },
+                    { title: "Custom", href: "/custom" },
+                ],
+            },
+            {
+                title: "Supervised Learning",
+                href: "/supervised-learning",
+                items: [
+                    { title: "Stepper", href: "/stepper" },
+                    { title: "Tabs", href: "/tabs" },
+                    { title: "Note", href: "/note" },
+                    { title: "Code Block", href: "/code-block" },
+                    { title: "Image & Link", href: "/image-link" },
+                    { title: "Custom", href: "/custom" },
+                ],
+            },
+            {
+                title: "Unsupervised Learning",
+                href: "/unsupervised-learning",
+                items: [
+                    { title: "Stepper", href: "/stepper" },
+                    { title: "Tabs", href: "/tabs" },
+                    { title: "Note", href: "/note" },
+                    { title: "Code Block", href: "/code-block" },
+                    { title: "Image & Link", href: "/image-link" },
+                    { title: "Custom", href: "/custom" },
+                ],
+            },
+            {
+                title: "Model Evaluation",
+                href: "/model-evaluation",
+                items: [
+                    { title: "Stepper", href: "/stepper" },
+                    { title: "Tabs", href: "/tabs" },
+                    { title: "Note", href: "/note" },
+                    { title: "Code Block", href: "/code-block" },
+                    { title: "Image & Link", href: "/image-link" },
+                    { title: "Custom", href: "/custom" },
+                ],
+            },
+        ],
+    },
+    {
+        title: "Deep Learning",
+        href: "/deep-learning",
+        noLink: true,
+        items: [
+            {
+                title: "Perceptron",
+                href: "/perceptron",
+                items: [
+                    { title: "Stepper", href: "/stepper" },
+                    { title: "Tabs", href: "/tabs" },
+                    { title: "Note", href: "/note" },
+                    { title: "Code Block", href: "/code-block" },
+                    { title: "Image & Link", href: "/image-link" },
+                    { title: "Custom", href: "/custom" },
+                ],
+            },
+            {
+                title: "CNN",
+                href: "/cnn",
+                items: [
+                    { title: "Stepper", href: "/stepper" },
+                    { title: "Tabs", href: "/tabs" },
+                    { title: "Note", href: "/note" },
+                    { title: "Code Block", href: "/code-block" },
+                    { title: "Image & Link", href: "/image-link" },
+                    { title: "Custom", href: "/custom" },
+                ],
+            },
+            {
+                title: "RNN",
+                href: "/rnn",
+                items: [
+                    { title: "Stepper", href: "/stepper" },
+                    { title: "Tabs", href: "/tabs" },
+                    { title: "Note", href: "/note" },
+                    { title: "Code Block", href: "/code-block" },
+                    { title: "Image & Link", href: "/image-link" },
+                    { title: "Custom", href: "/custom" },
+                ],
+            },
+            {
+                title: "Large Language Model",
+                href: "/llm",
+                items: [
+                    { title: "Stepper", href: "/stepper" },
+                    { title: "Tabs", href: "/tabs" },
+                    { title: "Note", href: "/note" },
+                    { title: "Code Block", href: "/code-block" },
+                    { title: "Image & Link", href: "/image-link" },
+                    { title: "Custom", href: "/custom" },
+                ],
+            },
+            {
+                title: "MLOPS",
+                href: "/mlops",
+                items: [
+                    { title: "Stepper", href: "/stepper" },
+                    { title: "Tabs", href: "/tabs" },
+                    { title: "Note", href: "/note" },
+                    { title: "Code Block", href: "/code-block" },
+                    { title: "Image & Link", href: "/image-link" },
+                    { title: "Custom", href: "/custom" },
+                ],
+            },
+        ],
+    },
+    {
+        title: "Natural Language Processing",
+        href: "/nlp",
+        noLink: true,
+        items: [
+            {
+                title: "Sentiment Analysis",
+                href: "/components",
+                items: [
+                    { title: "Stepper", href: "/stepper" },
+                    { title: "Tabs", href: "/tabs" },
+                    { title: "Note", href: "/note" },
+                    { title: "Code Block", href: "/code-block" },
+                    { title: "Image & Link", href: "/image-link" },
+                    { title: "Custom", href: "/custom" },
+                ],
+            },
+            {
+                title: "Text Preprocessing",
+                href: "/cnn",
+                items: [
+                    { title: "Stepper", href: "/stepper" },
+                    { title: "Tabs", href: "/tabs" },
+                    { title: "Note", href: "/note" },
+                    { title: "Code Block", href: "/code-block" },
+                    { title: "Image & Link", href: "/image-link" },
+                    { title: "Custom", href: "/custom" },
+                ],
+            },
+            {
+                title: "Semantic Search",
+                href: "/rnn",
+                items: [
+                    { title: "Stepper", href: "/stepper" },
+                    { title: "Tabs", href: "/tabs" },
+                    { title: "Note", href: "/note" },
+                    { title: "Code Block", href: "/code-block" },
+                    { title: "Image & Link", href: "/image-link" },
+                    { title: "Custom", href: "/custom" },
+                ],
+            },
+            {
+                title: "Large Language Model",
+                href: "/llm",
+                items: [
+                    { title: "Stepper", href: "/stepper" },
+                    { title: "Tabs", href: "/tabs" },
+                    { title: "Note", href: "/note" },
+                    { title: "Code Block", href: "/code-block" },
+                    { title: "Image & Link", href: "/image-link" },
+                    { title: "Custom", href: "/custom" },
+                ],
+            },
+        ],
+    },
+    {
+        title: "Reinforcement Learning",
+        href: "/rl",
+        noLink: true,
+        items: [
+            { title: "Introduction", href: "/introduction" },
+            { title: "Installation", href: "/installation" },
+            {
+                title: "Components",
+                href: "/components",
+                items: [
+                    { title: "Stepper", href: "/stepper" },
+                    { title: "Tabs", href: "/tabs" },
+                    { title: "Note", href: "/note" },
+                    { title: "Code Block", href: "/code-block" },
+                    { title: "Image & Link", href: "/image-link" },
+                    { title: "Custom", href: "/custom" },
+                ],
+            },
+        ],
+    },
+];
+
+export default ROUTES
