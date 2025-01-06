18cf3038314aefc86bc27e1f056300182724d76d
preszzz <107518358+preszzz@users.noreply.github.com>
2025-01-05 21:43:13 -0500

    Dev (#6)
    * update ml section
    
    * update math section
    
    * fix tiny issue

--- utils/routes-map.ts
+++ utils/routes-map.ts
@@ -168,7 +168,6 @@ const ROUTES: EachRoute[] = [
                 title: "Model Evaluation",
                 href: "/model-evaluation",
                 items: [
-
                     { title: "Introduction", href: "/introduction" },
                     { title: "Performance Metrics", href: "/performance-metrics" },
                     { title: "Classification Metrics", href: "/classification-metrics" },
