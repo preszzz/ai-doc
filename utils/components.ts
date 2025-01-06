590f777e3aae07634d5be3c1b06663eac6e6bea7
preszzz <107518358+preszzz@users.noreply.github.com>
2025-01-05 23:52:16 -0500

    small improvement

---
+++ utils/components.ts
@@ -0,0 +1,23 @@
+// custom components imports
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Pre from "@/components/markdown/pre";
+import Note from "@/components/markdown/note";
+import { Stepper, StepperItem } from "@/components/markdown/stepper";
+import Image from "@/components/markdown/image";
+import Link from "@/components/markdown/link";
+import Outlet from "@/components/markdown/outlet";
+
+// add custom components
+export const components = {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+  pre: Pre,
+  Note,
+  Stepper,
+  StepperItem,
+  img: Image,
+  a: Link,
+  Outlet,
+};
